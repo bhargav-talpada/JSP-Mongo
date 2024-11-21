65bcb65722b6898c940ec5180cd0e6bbe53061f2
Bhargav Talpada <119996397+bhargav-talpada@users.noreply.github.com>
2024-11-21 20:31:24 +0530

    Update ProjectionOp.js

--- ProjectionOp.js
+++ ProjectionOp.js
@@ -1,14 +1,14 @@
 // Projection Operator
 // 1. $[Positional Operator]
-//    syntax : { array:'value' },{array.$:1}
+   syntax : { array:'value' },{array.$:1}
 // 2. $elematch
-//    syntax : {array:'value'},{array.$elematch:'value'}
+   syntax : {array:'value'},{array.$elematch:'value'}
 // 3. $slice
-//    syntax : {array:'value'},{array:{$skills:[starting index, no of ele]}}
+   syntax : {array:'value'},{array:{$skills:[starting index, no of ele]}}
 
 
-// db.emp.find({skills:'html'},{_id:0, 'skills.$':1, ename:1})
+db.emp.find({skills:'html'},{_id:0, 'skills.$':1, ename:1})
 
-// db.emp.find({skills:'html'},{_id:0, ename:1, skills:{$elemMatch:{$eq:'node'}}})
+db.emp.find({skills:'html'},{_id:0, ename:1, skills:{$elemMatch:{$eq:'node'}}})
 
-// db.emp.find({skills:'html'},{_id:0, skills:{$slice:[0,3]}})
+db.emp.find({skills:'html'},{_id:0, skills:{$slice:[0,3]}})
